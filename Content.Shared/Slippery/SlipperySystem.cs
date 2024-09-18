d0b600e6fd129693df38676421233904cf4e72ce
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-09-18 20:30:24 +0300

    Update SlipperySystem.cs

--- Content.Shared/Slippery/SlipperySystem.cs
+++ Content.Shared/Slippery/SlipperySystem.cs
@@ -128,8 +128,6 @@ public sealed class SlipperySystem : EntitySystem
 
         _stun.TryParalyze(other, TimeSpan.FromSeconds(component.ParalyzeTime), true);
 
-        RaiseLocalEvent(other, new ParkSlipEvent(uid));
-
         // Preventing from playing the slip sound when you are already knocked down.
         if (playSound)
         {
@@ -164,8 +162,3 @@ public record struct SlipCausingAttemptEvent (bool Cancelled);
 /// <param name="Slipped">The entity being slipped</param>
 [ByRefEvent]
 public readonly record struct SlipEvent(EntityUid Slipped);
-
-/// <summary>
-///     This is an event raised on an entity after they slip. Duh.
-/// </summary>
-public readonly record struct ParkSlipEvent(EntityUid Tripper);
